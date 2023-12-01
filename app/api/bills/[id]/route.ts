b95c6e27b4bc51539bbcc54a05c0eb0b9910c1d7
Vithusan Gopalapillai <gvithusan@gmail.com>
2023-12-01 16:56:11 +0100

    Invoice ID Update

---
+++ app/api/bills/[id]/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+
+export async function PATCH(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+  ) {
+    const body = await request.json();
+    console.log(body);
+  
+    const bill = await prisma.bill.findUnique({
+      where: {
+        id: parseInt(params.id),
+      },
+    });
+    if (!bill)
+      return NextResponse.json({ error: "Invalid Invoice" }, { status: 404 });
+  
+    const updatedBill = await prisma.bill.update({
+      where: { id: bill.id },
+      data: {
+        createdAt: body.createdAt,
+        billnumber: parseInt(body.billnumber),
+        customerId: parseInt(body.customerId),
+        staffId: parseInt(body.staffId),
+      },
+    });
+  
+    return NextResponse.json(updatedBill);
+  }
+    
