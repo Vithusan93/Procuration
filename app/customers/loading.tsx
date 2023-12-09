1aff1048d464abca54f8707cad9d0600eee40070
Vithusan Gopalapillai <gvithusan@gmail.com>
2023-12-09 03:24:10 +0100

    skeleton try

--- app/customers/loading.tsx
+++ app/customers/loading.tsx
@@ -1,28 +1,15 @@
-import prisma from "@/prisma/client";
+import React from 'react'
 import Link from "next/link";
-import React from "react";
-import { Button, Table, Theme, Heading, Container } from "@radix-ui/themes";
+import { Table } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
-import delay from 'delay';
-import LoadingCustomerPage from "./loading";
-import CustomerAction from "./CustomerAction";
+import CustomerAction from './CustomerAction';
 
 
-const Customers = async () => {
-  const customers = await prisma.customer.findMany();
-  await delay(2000);
-
+const LoadingCustomerPage = () => {
+    const customers= [1,2,3,4];
   return (
-    <>
-      
-      
-        <Container size="4">
-          <div className="flex flex-col w-full">
-            <div className="bg-gray-200 w-full p-4">
-              <Heading className="text-gray-900">Customer Liste</Heading>
-            </div>
-          </div>
-          <Table.Root variant="surface">
+    <div> <Table.Root variant="surface">
     <Table.Header>
       <Table.Row>
         <Table.ColumnHeaderCell className="hidden md:table-cell">
@@ -41,35 +28,28 @@ const Customers = async () => {
     </Table.Header>
     <Table.Body>
       {customers.map((customer) => (
-        <Table.Row key={customer.id}>
+        <Table.Row key={customer}>
           <Table.Cell className="hidden md:table-cell">
-          <Link href={`/customers/${customer.id}`}>
-              {customer.firstname}
+          <Link href={`/customers/${customer}`}>
+              <Skeleton/>
             </Link>
           </Table.Cell>
           <Table.Cell className="hidden md:table-cell">
-            {customer.lastname}
+            <Skeleton/>
           </Table.Cell>
           <Table.Cell className="hidden md:table-cell">
-            {customer.email}
+            <Skeleton/>
           </Table.Cell>
           <Table.Cell className="hidden md:table-cell">
-            {customer.phone}
+            <Skeleton/>
           </Table.Cell>
         </Table.Row>
       ))}
     </Table.Body>
   </Table.Root>
-  <CustomerAction/>
-        </Container>
-          
-        
+ 
+  </div>
+  )
+}
 
-      
-    </>
-  );
-};
-export const dynamic = "force-dynamic";
-export const revalidate = false;
-
-export default Customers;
+export default LoadingCustomerPage
